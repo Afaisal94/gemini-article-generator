a8f858ec4cb677a7c4457a632776c5bce871a153
Afaisal94 <a.faisal1994@gmail.com>
2024-02-21 06:52:27 +0700

    Test Env Variable

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Gemini Article Generator'
+
+@app.route('/api')
+def api():
+    api_key = os.getenv("API_KEY")
+    return api_key
